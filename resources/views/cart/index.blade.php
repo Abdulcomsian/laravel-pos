6c7f3b915d94e9fb027669b216aa0701ef0e8237
obiafridi <obaid.afridi@accrualhub.com>
2022-10-24 16:44:29 +0500

    changes done

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -6,44 +6,44 @@
 <style>
     @media print {
          td,
-    th,
-    tr,
-    table {
-        border-top: 1px solid black;
-        border-collapse: collapse;
-    }
+        th,
+        tr,
+        table {
+            border-top: 1px solid black;
+            border-collapse: collapse;
+        }
 
 
 
-    td.description,
-    th.description {
-        width: 100px;
-        max-width: 100px;
-    }
+        td.description,
+        th.description {
+            width: 100px;
+            max-width: 100px;
+        }
 
-    td.quantity,
-    th.quantity {
-        width: 40px;
-        max-width: 40px;
-        word-break: break-all;
-    }
+        td.quantity,
+        th.quantity {
+            width: 40px;
+            max-width: 40px;
+            word-break: break-all;
+        }
 
-    td.price,
-    th.price {
-        width: 80px;
-        max-width: 80px;
-        word-break: break-all;
-    }
+        td.price,
+        th.price {
+            width: 80px;
+            max-width: 80px;
+            word-break: break-all;
+        }
 
-    .centered {
-        text-align: center;
-        align-content: center;
-    }
+        .centered {
+            text-align: center;
+            align-content: center;
+        }
 
-    .ticket {
-        width: 500px;
-        max-width: 500px;
-    }
+        .ticket {
+            width: 210px;
+            max-width: 210px;
+        }
       }
     
 </style>
